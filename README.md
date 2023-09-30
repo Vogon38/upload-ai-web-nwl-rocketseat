ef753428177708933ec99aa243bacbfa4b36ce5d
Luana Cristini <luana@thecyborg.xyz>
2023-09-30 17:07:16 -0300

    variables now are getting the value from prisma db

---
+++ README.md
@@ -0,0 +1,20 @@
+# upload.ai
+
+## Installation
+
+1. Clone the repository.
+2. Install dependencies with `pnpm install`.
+
+## Usage
+
+1. Start the development server with `pnpm run dev`.
+
+## Features
+
+- Input a prompt and generate AI-generated text based on that prompt.
+- Select a video to transcribe and use the transcription in the prompt.
+- Set various options for the AI model, such as temperature.
+
+## About
+
+This project was developed as part of the NWL 💜 event by Rocketseat.
--- src/components/video-input-form.tsx
+++ src/components/video-input-form.tsx
@@ -6,9 +6,25 @@ import { Separator } from "./ui/separator"
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react"
 import { getFFmpeg } from "@/lib/ffmpeg"
 import { fetchFile } from "@ffmpeg/util"
+import { api } from "@/lib/axios"
 
-export function VideoInputForm() {
+type Status = "waiting" | "converting" | "uploading" | "generating" | "success"
+
+const statusMessages = {
+    converting: "Convertendo...",
+    generating: "Transcrevendo...",
+    uploading: "Enviando...",
+    success: "Sucesso!"
+}
+
+interface VideoInputFormProps {
+    onVideoUploaded: (id: string) => void
+}
+
+export function VideoInputForm(props: VideoInputFormProps) {
     const [videoFile, setVideoFile] = useState<File | null>(null)
+    const [status, setStatus] = useState<Status>("waiting")
+
     const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
     function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
@@ -30,10 +46,6 @@ export function VideoInputForm() {
 
         await ffmpeg.writeFile("input.mp4", await fetchFile(video))
 
-        // ffmpeg?.on("log", log => {
-        //     console.log(log)
-        // })
-
         ffmpeg.on("progress", progress => {
             console.log("Convert progress: " + Math.round(progress.progress * 100))
         })
@@ -52,7 +64,7 @@ export function VideoInputForm() {
 
         const data = await ffmpeg.readFile("output.mp3")
 
-        const audioFileBlob = new Blob([data], {type: "audio/mpeg"})
+        const audioFileBlob = new Blob([data], { type: "audio/mpeg" })
         const audioFile = new File([audioFileBlob], "audio.mp3", {
             type: "audio/mpeg"
         })
@@ -71,8 +83,29 @@ export function VideoInputForm() {
             return
         }
 
+        setStatus("converting")
+
         const audioFile = await convertVideoToAudio(videoFile)
-        console.log(audioFile, prompt)
+
+        const data = new FormData()
+
+        data.append("file", audioFile)
+
+        setStatus("uploading")
+
+        const response = await api.post("/videos", data)
+
+        const videoId = response.data.video.id
+
+        setStatus("generating")
+
+        await api.post(`/videos/${videoId}/transcription`, {
+            prompt
+        })
+
+        setStatus("success")
+
+        props.onVideoUploaded(videoId)
     }
 
     const previewURL = useMemo(() => {
@@ -82,41 +115,50 @@ export function VideoInputForm() {
         return URL.createObjectURL(videoFile)
     }, [videoFile])
 
-    return(
+    return (
         <form onSubmit={handleUploadVideo} className="space-y-6">
-        <label
-            htmlFor="video"
-            className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5 mt-4"
-        >
-            {previewURL ? (
-                <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0" />
-            ) : (
-                <>
-                <FileVideo className="w-4 h-4" />
-                Selecione um vídeo
-                </>
-            )}
-        </label>
+            <label
+                htmlFor="video"
+                className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5 mt-4"
+            >
+                {previewURL ? (
+                    <video src={previewURL} controls={false} className="pointer-events-none absolute inset-0" />
+                ) : (
+                    <>
+                        <FileVideo className="w-4 h-4" />
+                        Selecione um vídeo
+                    </>
+                )}
+            </label>
 
-        <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
+            <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
 
-        <Separator className="mt-6" />
+            <Separator className="mt-6" />
 
-        <div className="space-y-2 mt-6">
-            <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-            <Textarea
-                ref={promptInputRef}
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
-            />
-        </div>
+            <div className="space-y-2 mt-6">
+                <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+                <Textarea
+                    ref={promptInputRef}
+                    disabled={status !== "waiting" && status !== "success"}
+                    id="transcription_prompt"
+                    className="h-20 leading-relaxed resize-none"
+                    placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"
+                />
+            </div>
 
-        <Button type="submit" className="w-full">
-            Carregar vídeo
-            <Upload className="w-4 h-4 ml-2" />
-        </Button>
-    </form>
+            <Button 
+                data-success={status === "success"}
+                disabled={status !== "waiting" && status !== "success"} 
+                type="submit" 
+                className="w-full data-[success=true]:bg-emerald-400">
+                {status === "waiting" ? (
+                    <>
+                        Carregar vídeo
+                        <Upload className="w-4 h-4 ml-2" />
+                    </>
+                ) : statusMessages[status]}
+            </Button>
+        </form>
     )
-    
+
 }
